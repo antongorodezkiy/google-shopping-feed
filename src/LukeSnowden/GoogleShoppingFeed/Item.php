1e8bb7dee53abc563ed9e672e9a8e09db349b1b2
Eduardo K <eduard@kozachek.net>
2017-09-20 22:03:26 +0300

    Added safe encoding with return htmlentities()

--- src/LukeSnowden/GoogleShoppingFeed/Item.php
+++ src/LukeSnowden/GoogleShoppingFeed/Item.php
@@ -502,10 +502,7 @@ class Item
      */
     private function safeCharEncodeURL($string)
     {
-        return str_replace(
-            array('%', '[', ']', '{', '}', '|', ' ', '"', '<', '>', '#', '\\', '^', '~', '`'),
-            array('%25', '%5b', '%5d', '%7b', '%7d', '%7c', '%20', '%22', '%3c', '%3e', '%23', '%5c', '%5e', '%7e', '%60'),
-        $string);
+        return htmlentities($string, ENT_QUOTES, 'UTF-8');
     }
 
     /**
@@ -514,10 +511,7 @@ class Item
      */
     private function safeCharEncodeText($string)
     {
-        return str_replace(
-            array('•', '”', '“', '’', '‘', '™', '®', '°', "\n"),
-            array('&#8226;', '&#8221;', '&#8220;', '&#8217;', '&#8216;', '&trade;', '&reg;', '&deg;', ''),
-        $string);
+        return htmlentities($string, ENT_QUOTES, 'UTF-8');
     }
 
     /**
